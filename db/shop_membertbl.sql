f006685ff5c461fd3a5fbe37533b9c9926da7a27
Dawn Jang <128432201+absolutelydawn@users.noreply.github.com>
2024-03-21 11:24:40 +0900

    Create shop_membertbl.sql
    shop_membertbl.sql

---
+++ db/shop_membertbl.sql
@@ -0,0 +1,36 @@
+--------------------------------------------------------
+--  颇老捞 积己凳 - 格夸老-3岿-21-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table MEMBERTBL
+--------------------------------------------------------
+
+  CREATE TABLE "SHOP"."MEMBERTBL" 
+   (	"MEMBERID" CHAR(8 BYTE) DEFAULT NULL, 
+	"MEMBERNAME" NCHAR(5) DEFAULT NULL, 
+	"MEMBERADDRESS" NVARCHAR2(20) DEFAULT NULL
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Trigger TRG_DELETEDMEMBERTBL
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "SHOP"."TRG_DELETEDMEMBERTBL" 
+    after delete
+    on membertbl
+    for each row
+begin
+    insert into deletedmembertbl
+    values (:old.memberid, :old.membername, :old.memberaddress, sysdate());
+end;
+/
+ALTER TRIGGER "SHOP"."TRG_DELETEDMEMBERTBL" ENABLE;
+--------------------------------------------------------
+--  Constraints for Table MEMBERTBL
+--------------------------------------------------------
+
+  ALTER TABLE "SHOP"."MEMBERTBL" MODIFY ("MEMBERNAME" NOT NULL ENABLE);
+  ALTER TABLE "SHOP"."MEMBERTBL" MODIFY ("MEMBERID" NOT NULL ENABLE);
