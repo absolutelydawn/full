4773a41b1d80c5ad4933970c0ee4cd83d54d2dd6
absolutelydawn <absolutelydawnn@gmail.com>
2024-03-25 14:40:19 +0900

    hello.js

--- node/hello/hello.js
+++ node/hello/hello.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send("Hello Node Js~!!")
+    res.send("Hello Node Js~!!!!")
 });
 
 app.listen(8000, function(){
