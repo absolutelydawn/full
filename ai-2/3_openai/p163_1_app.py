a768a890b7339e49d819b90a4e9cac997cc0d290
absolutelydawn <absolutelydawnn@gmail.com>
2024-06-18 00:31:46 +0000

    openai

--- ai-2/3_openai/p163_1_app.py
+++ ai-2/3_openai/p163_1_app.py
@@ -1,25 +1,31 @@
 import openai
 import json
 
-def response_from_ChatAI(user_content, r_num = 1):
+def response_from_ChatAI(user_content, r_num=1):
     messages = [
         {"role": "user", "content": user_content}
     ]
 
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = messages,
-        max_tokens = 1000,
-        temperature = 0.8,
-        n = r_num
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=1000,
+        temperature=0.8,
+        n=r_num
     )
 
-    assistent_replies = []
+    assistant_replies = []
 
     for choice in response.choices:
-        assistent_replies = choice.message['content']
+        assistant_replies.append(choice.message['content'])
 
-    return assistent_replies
+    return assistant_replies
 
-res = response_from_ChatAI("대한민국 헌법 1조 1항은?")
-print(res)
+while True:
+    user_input = input("Q : ")
+    if user_input.lower() in ['q', 'ㅂ']:
+        break
+    else:
+        res = response_from_ChatAI(user_input)
+        for reply in res:
+            print("A : ", reply, end="\n\n")
